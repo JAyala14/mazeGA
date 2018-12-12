7ac35ec1bf7e0de3fb515715e0e30851893a2c60
Jonathan Ayala <j.ayala141@csu.fullerton.edu>
2018-12-11 18:25:17 -0800

    Made changes to test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import random
 import geneticFunctions as gf
+from pacman import Game
 #Define max population
 MAXPOP = 15
 #Define mutation rate
@@ -7,36 +8,18 @@ MUTATIONRATE = 0.1
 #Array of instances of run
 populationArray = []
 
-populationArray = gf.initializePopulation(MAXPOP)
+gamePlay = Game()
+gamePlay.play()
 
-print(populationArray[5].instructions, '\n')
-
-gf.populationMutation(populationArray, MUTATIONRATE)
-
-print(populationArray[5].instructions, '\n')
-
+#populationArray = gf.initializePopulation(MAXPOP)
+#print(populationArray[5].instructions, '\n')
+#gf.populationMutation(populationArray, MUTATIONRATE)
+#print(populationArray[5].instructions, '\n')
 
 #parent1 = populationArray[5]
 #parent2 = populationArray[6]
 
-
 #print (parent1.instructions, '\n')
 #print (parent2.instructions, '\n')
-
 #child = gf.singleCrossover(parent1, parent2)
-
 #print (child.instructions, '\n')
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-#for i in range(MAXPOP):
-#    print(populationArray[i].instructions, '\n')
-=======
->>>>>>> 07cdd02c50f54f8c83b9b4d5dbe53b953f490cd7
